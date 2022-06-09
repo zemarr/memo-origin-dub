c9019e3fee64658d03be74eb71aba1ba6ce25779
Chukwu <chukwudaluay@gmail.com>
2022-06-09 13:12:22 +0100

    center content on document

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,116 +1,118 @@
 <template>
-  <div class="home bg-faintGrey min-h-screen p-8">
-    <div class="overview bg-white rounded-[4px] px-8 py-9">
-      <div class="flex items-start md:flex-row flex-col w-full mb-3">
-        <div class="logo w-[80px] h-[80px] mr-4">
-          <img src="../assets/images/netflix-logo.png" alt="">
+  <main>
+    <div class="home bg-faintGrey min-h-screen p-8 max-w-[894px] mx-auto">
+      <div class="overview bg-white rounded-[4px] px-8 py-9">
+        <div class="flex items-start md:flex-row flex-col w-full mb-3">
+          <div class="logo w-[80px] h-[80px] mr-4">
+            <img src="../assets/images/netflix-logo.png" alt="">
+          </div>
+          <div class="relative w-[inherit]">
+            <div class="header flex items-center justify-between">
+              <h1 class="text-[24px] leading-[32px] mb-[6px] tracking-[-2%]">The Netflix Memo 📌📌</h1>
+              <img src="../assets//images/kinfolk-black.svg" alt="" class="w-[24px] h-[21.06px]">
+            </div>
+            <h2 class="mb-[12px] font-[500] text-[16px] leading-[24px]">Netflix</h2>
+            <p class="flex items-center text-greyText mb-[20px] text-[13px] leading-[20px]">
+              <span class="mr-[6px]">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M8 1C5.23969 1 3 3.01594 3 5.5C3 9.5 8 15 8 15C8 15 13 9.5 13 5.5C13 3.01594 10.7603 1 8 1ZM8 8C7.60444 8 7.21776 7.8827 6.88886 7.66294C6.55996 7.44318 6.30362 7.13082 6.15224 6.76537C6.00087 6.39991 5.96126 5.99778 6.03843 5.60982C6.1156 5.22186 6.30608 4.86549 6.58579 4.58579C6.86549 4.30608 7.22186 4.1156 7.60982 4.03843C7.99778 3.96126 8.39991 4.00087 8.76537 4.15224C9.13082 4.30362 9.44318 4.55996 9.66294 4.88886C9.8827 5.21776 10 5.60444 10 6C9.99942 6.53026 9.78852 7.03863 9.41357 7.41357C9.03863 7.78852 8.53026 7.99942 8 8Z" fill="#666666"/>
+                </svg>
+              </span>
+              Netflix Corporate Headquarters 100 Winchester Circle Los Gatos, CA 95032
+            </p>
+            <div class="flex items-center flex-wrap">
+              <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
+                Year founded: <span class="text-black">2012</span>
+              </div>
+              <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
+                Industry: <span class="text-black">Entertainment</span>
+              </div>
+              <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
+                Business model: <span class="text-black">B2C</span>
+              </div>
+              <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
+                No of employees: <span class="text-black">58</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="relative w-[inherit]">
-          <div class="header flex items-center justify-between">
-            <h1 class="text-[24px] leading-[32px] mb-[6px] tracking-[-2%]">The Netflix Memo 📌📌</h1>
-            <img src="../assets//images/kinfolk-black.svg" alt="" class="w-[24px] h-[21.06px]">
-          </div>
-          <h2 class="mb-[12px] font-[500] text-[16px] leading-[24px]">Netflix</h2>
-          <p class="flex items-center text-greyText mb-[20px] text-[13px] leading-[20px]">
-            <span class="mr-[6px]">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8 1C5.23969 1 3 3.01594 3 5.5C3 9.5 8 15 8 15C8 15 13 9.5 13 5.5C13 3.01594 10.7603 1 8 1ZM8 8C7.60444 8 7.21776 7.8827 6.88886 7.66294C6.55996 7.44318 6.30362 7.13082 6.15224 6.76537C6.00087 6.39991 5.96126 5.99778 6.03843 5.60982C6.1156 5.22186 6.30608 4.86549 6.58579 4.58579C6.86549 4.30608 7.22186 4.1156 7.60982 4.03843C7.99778 3.96126 8.39991 4.00087 8.76537 4.15224C9.13082 4.30362 9.44318 4.55996 9.66294 4.88886C9.8827 5.21776 10 5.60444 10 6C9.99942 6.53026 9.78852 7.03863 9.41357 7.41357C9.03863 7.78852 8.53026 7.99942 8 8Z" fill="#666666"/>
-              </svg>
-            </span>
-            Netflix Corporate Headquarters 100 Winchester Circle Los Gatos, CA 95032
-          </p>
-          <div class="flex items-center flex-wrap">
-            <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
-              Year founded: <span class="text-black">2012</span>
-            </div>
-            <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
-              Industry: <span class="text-black">Entertainment</span>
-            </div>
-            <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
-              Business model: <span class="text-black">B2C</span>
-            </div>
-            <div class="bg-faintGrey text-greyText text-[14px] leading-[20px] py-[6px] px-[12px] max-w-[max-content] min-w-[122.59px] mr-3 mb-3 rounded-[4px]">
-              No of employees: <span class="text-black">58</span>
-            </div>
-          </div>
+        <hr>
+        <div class="pt-6">
+          <h3 class="font-[500] text-[16px] leading-[24px] mb-3">Company Overview</h3>
+          <p class="text-[14px] leading-[26px] text-greyText">Netflix is a pay television over-the-top media service and American production company that offers subscription-based video on demand from a library of films and television series, 40% of which is original programming produced in-house.</p>
         </div>
       </div>
-      <hr>
-      <div class="pt-6">
-        <h3 class="font-[500] text-[16px] leading-[24px] mb-3">Company Overview</h3>
-        <p class="text-[14px] leading-[26px] text-greyText">Netflix is a pay television over-the-top media service and American production company that offers subscription-based video on demand from a library of films and television series, 40% of which is original programming produced in-house.</p>
-      </div>
-    </div>
 
-    <div class="relative">
-      <div class="bg-white rounded-[4px] p-8 my-8">
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Team
+      <div class="relative">
+        <div class="bg-white rounded-[4px] p-8 my-8">
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Team
+            </div>
+            <ul class="mb-6 list-disc ml-4 text-greyText text-[14px]">
+              <li>Toni Campbell - Founder</li>
+              <li>Jay Freeman - Co Founder</li>
+              <li>Faithful Johnson - Developer</li>
+            </ul>
           </div>
-          <ul class="mb-6 list-disc ml-4 text-greyText text-[14px]">
-            <li>Toni Campbell - Founder</li>
-            <li>Jay Freeman - Co Founder</li>
-            <li>Faithful Johnson - Developer</li>
-          </ul>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Market Opportunity
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Market Opportunity
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">Identify market leaders & what works for them. Improve your online visibility. Sell More. Understand market potential. Spot trends. Benchmark yourself against the market & rivals.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">Identify market leaders & what works for them. Improve your online visibility. Sell More. Understand market potential. Spot trends. Benchmark yourself against the market & rivals.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Customer & Pricing
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Customer & Pricing
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">Customer-driven pricing is the practice of setting prices according to customers' perceived value of a company's goods or services.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">Customer-driven pricing is the practice of setting prices according to customers' perceived value of a company's goods or services.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Product
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Product
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">Define the core purpose. Let us work out the purpose of our product by asking a set of simple but revealing questions.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">Define the core purpose. Let us work out the purpose of our product by asking a set of simple but revealing questions.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Customers Acquisition & Retention
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Customers Acquisition & Retention
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">While customer acquisition is the act of gaining new customers, customer retention is focused on developing better relationships with your existing customers.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">While customer acquisition is the act of gaining new customers, customer retention is focused on developing better relationships with your existing customers.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Summary Financials
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Summary Financials
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">It is the process of presenting the final value of all classified account in a separate statement, called “Trial Balance”.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">It is the process of presenting the final value of all classified account in a separate statement, called “Trial Balance”.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Deal
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Deal
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">Deal memos are the precursor to a full contract, which ultimately documents the legal agreement between the parties.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">Deal memos are the precursor to a full contract, which ultimately documents the legal agreement between the parties.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Outcomes Analysis
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Outcomes Analysis
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">Outcome analysis allows the transplant team to understand the effects of their practice and improve quality.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">Outcome analysis allows the transplant team to understand the effects of their practice and improve quality.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Conclusion
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Conclusion
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">The conclusion of a memo should not simply provide a summary of the memo's entire contents, but it should be a true conclusion—that is, an articulated conviction arrived at on the basis of the evidence presented.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">The conclusion of a memo should not simply provide a summary of the memo's entire contents, but it should be a true conclusion—that is, an articulated conviction arrived at on the basis of the evidence presented.</p>
-        </div>
-        <div class="relative">
-          <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
-            Appendix
+          <div class="relative">
+            <div class="bg-faintGrey px-4 py-[6px] text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]">
+              Appendix
+            </div>
+            <p class="mb-6 text-greyText text-[14px]">This is where you put everything else—all the components and processes that got you to your final results and recommendations.</p>
           </div>
-          <p class="mb-6 text-greyText text-[14px]">This is where you put everything else—all the components and processes that got you to your final results and recommendations.</p>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
