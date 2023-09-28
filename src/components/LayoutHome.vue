981b1d9e44a87908d99473d6053d6896e72ee580
Chukwu <chukwudaluay@gmail.com>
2023-09-28 12:30:04 +0100

    fix sidebar bugs

--- src/components/LayoutHome.vue
+++ src/components/LayoutHome.vue
@@ -1,69 +1,90 @@
 <template>
-  <div class="">
-    <div class="fixed h-screen top-0 p-4 min-w-[max-content] w-auto bg-faintGrey text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]  ">
-    <div class="">
-      <botton class="fixed right-430 cursor-pointer" @click="showComponent = !showComponent" >
-        <img src="../assets/images/svgs/align-right.svg" alt="Image" class="w-[24px] h-[24px] mb-4" />
-      </botton>
+   <div class="">
+    <div class="left fixed h-screen -top-1 p-8 min-w-[max-content] w-auto backdrop-blur-lg text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px] transition-all shadow-lg z-[1000]" :class="(!showSidebar && windowWidth <= 1023) ? '-translate-x-[340px]' : 'translate-x-0'">
+      <button @click="showSidebar = !showSidebar" class="absolute -right-[52px] backdrop-blur-lg backdrop-opacity-80 py-3 px-2 flex items-center justify-center shadow-md rounded-tr-md rounded-br-md z-[1000] lg:hidden">
+        <span v-show="!showSidebar" class="expand block w-7">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="expand"><g data-name="Layer 2"><path d="M20 5a1 1 0 0 0-1-1h-5a1 1 0 0 0 0 2h2.57l-3.28 3.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L18 7.42V10a1 1 0 0 0 1 1 1 1 0 0 0 1-1zm-9.29 8.29a1 1 0 0 0-1.42 0L6 16.57V14a1 1 0 0 0-1-1 1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 0-2H7.42l3.29-3.29a1 1 0 0 0 0-1.42z" data-name="expand"></path></g></svg>
+        </span>
+        <span v-show="showSidebar" class="collapse block w-7">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="collapse"><g data-name="Layer 2"><path d="M19 9h-2.58l3.29-3.29a1 1 0 1 0-1.42-1.42L15 7.57V5a1 1 0 0 0-1-1 1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 0-2zm-9 4H5a1 1 0 0 0 0 2h2.57l-3.28 3.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L9 16.42V19a1 1 0 0 0 1 1 1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1z" data-name="collapse"></path></g></svg>
+        </span>
+      </button>
+      <div class="flex items-center">
+        <img src="../assets//images/kinfolk-black.svg" alt="" class="w-[34px] h-[50px] mr-3 mb-4">
+        <!-- <h2 class="text-[30px] leading-[48px]">Memo</h2> -->
       </div>
-     <div class="flex items-center mt-10">
-       <!-- <h2 class="text-[30px] leading-[48px]">Memo</h2> -->
-     </div>
-      <ul class=" fixed h-screen shadow-lg w-[max-content] z-[1000] transition-all translate-x-[-360px] md:translate-x-0'" :class="showComponent ? 'translate-x-[-360px]' : 'translate-x-0' ">
-      <li v-for="item in keyList" :key="item.id" @click="goAnchor(item)" class="cursor-pointer text-slate-500 text-[10px] font-normal leading-[24px] rounded-[4px] mb-[8px] hover:underline hover:text-black">{{ item.name }}</li>
-     </ul>
+      <ul>
+        <li v-for="item in keyList" :key="item.id" @click="goAnchor(item)" class="cursor-pointer text-slate-500 text-[18px] font-normal leading-[24px] rounded-[4px] mb-[8px]">{{ item.name }}</li>
+      </ul>
+    </div>
+    <div class="right" :class="windowWidth <= 1023 ? 'ml-0' : 'ml-[340px]'">
+      <slot></slot>
     </div>
    </div>
-    <div class="ml-80 w-3/4" :class="showComponent ? 'translate-x-[-140px]' : 'translate-x-0'">
-     <slot></slot>
-   </div>
 </template>
-<script   setup>
+<script>
 import { ref } from 'vue'
 
-const keyList = ref([
-  {
-    name: 'Team',
-    id: 'team'
-  },
-  {
-    name: 'Customer & Pricing',
-    id: 'customer_pricing'
-  },
-  {
-    name: 'Product',
-    id: 'product'
-  },
-  {
-    name: 'Customers Acquisition & Retention',
-    id: 'customers_cquisition_retention'
-  },
-  {
-    name: 'Summary Financials',
-    id: 'summary_financial'
-  },
-  {
-    name: 'Deal',
-    id: 'deal'
-  },
-  {
-    name: 'Outcomes Analysis',
-    id: 'outcomes_analysis'
-  },
-  {
-    name: 'Conclusion',
-    id: 'conclusion'
-  },
-  {
-    name: 'Appendix',
-    id: 'appendix'
-  }
-])
-const showComponent = ref(false)
-const goAnchor = (item) => {
-  const anchorElement = document.getElementById(item.id)
-  if (anchorElement) {
-    anchorElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
+export default {
+  setup () {
+    const showSidebar = ref(false)
+    const windowWidth = ref(window.innerWidth)
+    const keyList = ref([
+      {
+        name: 'Team',
+        id: 'team'
+      },
+      {
+        name: 'Customer & Pricing',
+        id: 'customer_pricing'
+      },
+      {
+        name: 'Product',
+        id: 'product'
+      },
+      {
+        name: 'Customers Acquisition & Retention',
+        id: 'customers_cquisition_retention'
+      },
+      {
+        name: 'Summary Financials',
+        id: 'summary_financial'
+      },
+      {
+        name: 'Deal',
+        id: 'deal'
+      },
+      {
+        name: 'Outcomes Analysis',
+        id: 'outcomes_analysis'
+      },
+      {
+        name: 'Conclusion',
+        id: 'conclusion'
+      },
+      {
+        name: 'Appendix',
+        id: 'appendix'
+      }
+    ])
+    const goAnchor = (item) => {
+      const anchorElement = document.getElementById(item.id)
+      closeSidebar()
+      if (anchorElement) {
+        anchorElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      }
+    }
+    const closeSidebar = () => {
+      showSidebar.value = false
+    }
+    return {
+      keyList,
+      goAnchor,
+      showSidebar,
+      closeSidebar,
+      windowWidth
+    }
   }
 }
+
 </script>
