7ff66d576ad05176e961b45fb283423bf5cddabe
Yu <yc48u22@soton.ac.uk>
2023-09-28 20:16:27 +0100

    Mobile Adaptation

--- src/components/LayoutHome.vue
+++ src/components/LayoutHome.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="">
-    <div class="fixed h-screen top-0 p-4 min-w-[max-content] w-auto bg-faintGrey text-[16px] font-[500] leading-[24px] rounded-[4px] mb-[8px]  ">
-    <div class="">
-      <botton class="fixed right-430 cursor-pointer" @click="showComponent = !showComponent" >
+  <div class="flex flex-row justify-center ">
+   <div class="left p-8 bg-white   h-screen top-0 w-[max-content] z-[1000] transition-all shadow-lg translate-x-[-360px] md:translate-x-0"   :class="showComponent ? 'translate-x-[-380px]' : 'translate-x-0'">
+     <div class="flex items-center">
+       <img src="../assets//images/kinfolk-black.svg" alt="" class="w-[34px] h-[50px] mr-3 mb-4 bg-white">
+     </div>
+     <ul class="w-auto" >
+      <li v-for="item in keyList" :key="item.id" @click="goAnchor(item)" class="cursor-pointer text-sm my-4 leading-5 text-slate-500 hover:underline hover:text-black">{{ item.name }}</li>
+     </ul>
+   </div>
+    <div class="right ml-0">
+      <botton class="absolute right-[24px] top-[4px] cursor-pointer" @click="showComponent = !showComponent" >
         <img src="../assets/images/svgs/align-right.svg" alt="Image" class="w-[24px] h-[24px] mb-4" />
       </botton>
-      </div>
-     <div class="flex items-center mt-10">
-       <!-- <h2 class="text-[30px] leading-[48px]">Memo</h2> -->
-     </div>
-      <ul class=" fixed h-screen shadow-lg w-[max-content] z-[1000] transition-all translate-x-[-360px] md:translate-x-0'" :class="showComponent ? 'translate-x-[-360px]' : 'translate-x-0' ">
-      <li v-for="item in keyList" :key="item.id" @click="goAnchor(item)" class="cursor-pointer text-slate-500 text-[10px] font-normal leading-[24px] rounded-[4px] mb-[8px] hover:underline hover:text-black">{{ item.name }}</li>
-     </ul>
-    </div>
-   </div>
-    <div class="ml-80 w-3/4" :class="showComponent ? 'translate-x-[-140px]' : 'translate-x-0'">
      <slot></slot>
    </div>
+  </div>
+  <!-- w-[max-content] z-[1000] transition-all shadow-sm translate-x-[-360px] md:translate-x-0' -->
 </template>
 <script   setup>
 import { ref } from 'vue'
@@ -59,7 +58,7 @@ const keyList = ref([
     id: 'appendix'
   }
 ])
-const showComponent = ref(false)
+const showComponent = ref(true)
 const goAnchor = (item) => {
   const anchorElement = document.getElementById(item.id)
   if (anchorElement) {
